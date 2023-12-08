a978e5bc2d367b3fb40cc8d5094f1c076ce50c2e
dc-base <dc-base@github.com>
2023-12-08 15:36:01 -0600

    day3

--- day3/day3.go
+++ day3/day3.go
@@ -4,9 +4,10 @@ import(
 	"bufio"
 	"fmt"
 	"os"
+	"unicode"
 	"regexp"
 	"strconv"
-	"strings"
+	//"strings"
 	)
 //var values = make(map[string]int)
 //func loadValues(){
@@ -22,30 +23,183 @@ import(
 //	}
 //	fmt.Println(values)
 //}
+// symbols := make(map[byte]bool{
+//     "*": true,
+//     "+":   true,
+//     "@": true,
+//     "$": true,
+// 	"/": true,
+// 	"-": true,
+// 	"%": true,
+// 	"&": true,
+// 	"#": true,
+// 	"=": true,
+// })
+func isSymbol(val rune) bool {
+	if(val != '.' && unicode.IsSymbol(val)){
+		return true
+	}
+	return false
+}
+func makeLineMap(line string) map[int]bool {
+	symbolPatt := `[*+@$\-%&#=\/]`
+	//symbolPatt := `[^\w\s.:w]`
+	reSym := regexp.MustCompile(symbolPatt)
+	symbols := reSym.FindAllStringIndex(line, -1)
+	symMap := make(map[int]bool)
+	for _, symbol := range symbols{
+		symMap[symbol[0]] = true
+	}
+	return symMap
+}
+func part1(lines []string){
+	decPatt := `\d+`
+	symbolPatt := `[*+@$\-%&#=\/]`
+	reDec := regexp.MustCompile(decPatt)
+	reSym := regexp.MustCompile(symbolPatt)
+	var sum int = 0
+	for i, line := range lines {
+		values := reDec.FindAllStringIndex(line, -1)
+		symbols := reSym.FindAllStringIndex(line, -1)
+		symMap := make(map[int]bool)
+		for _, symbol := range symbols{
+			symMap[symbol[0]] = true
+			//fmt.Println("sym")
+			//fmt.Println(symbol)
+
+		}
+		fmt.Println(symMap)
+		fmt.Println(values)
+		for _, value := range values {
+			dec := line[value[0]:value[1]]
+			//fmt.Println(dec)
+			_ = i
+
+			if value[0] != 0 {
+				if symMap[value[0]-1] {
+					fmt.Println(dec)
+					fmt.Println("Left")
+					temp, err := strconv.Atoi(dec)
+					if err != nil {
+						fmt.Println(err)
+					}
+					sum+=temp
+					continue
+				}
+			}
+			if(value[1] != len(line)-1){
+				if symMap[value[1]] {
+					fmt.Println(dec)
+					fmt.Println("Right")
+					temp, err := strconv.Atoi(dec)
+					if err != nil {
+						fmt.Println(err)
+					}
+					sum+=temp
+					continue
+				}
+			}
+			//first line of file
+			if i == 0{
+				start := value[0] - 1
+				if start < 0 {
+					start = 0
+				}
+				end := value[1] + 1
+				if end >= len(line) {
+					end = len(line) - 1
+				}
+				nextLine := makeLineMap(lines[i+1])
+				for start < end {
+					if nextLine[start]{
+						fmt.Println(dec)
+						fmt.Println("FL adj")
+						temp, err := strconv.Atoi(dec)
+						if err != nil {
+							fmt.Println(err)
+						}
+						sum+=temp
+					}
+					start++
+				}
+				continue
+			} else if i == len(lines) - 1{ //handle last line
+				start := value[0] - 1
+				if start < 0 {
+					start = 0
+				}
+				end := value[1] + 1
+				if end >= len(line) {
+					end = len(line) - 1
+				}
+				prevLine := makeLineMap(lines[len(lines)-2])
+				for start < end {
+					if prevLine[start]{
+						fmt.Println(dec)
+						fmt.Println("LL adj")
+						temp, err := strconv.Atoi(dec)
+						if err != nil {
+							fmt.Println(err)
+						}
+						sum+=temp
+					}
+					start++
+				}
+				continue
+			} else { //default case 
+				start := value[0] - 1
+				if start < 0 {
+					start = 0
+				}
+				end := value[1] + 1
+				if end >= len(line) {
+					end = len(line) - 1
+				}
+				nextLine := makeLineMap(lines[i+1])
+				prevLine := makeLineMap(lines[i-1]) 
+				for start < end {
+					if nextLine[start] || prevLine[start]{
+						fmt.Println(dec)
+						fmt.Println("MID adj")
+						temp, err := strconv.Atoi(dec)
+						if err != nil {
+							fmt.Println(err)
+						}
+						sum+=temp
+					}
+					start++
+				}
+				continue
+			}
+
+			//default case
+
+			
+			//start := value[0]
+			//for start < value[1] {
+				
+			//}
+		}
+
+
+	}
+	fmt.Println("Sum=%d", sum)
+
+	
+}
 func main(){
 	//readFile, err := os.Open("small.txt")
-	readFile, err := os.Open("data.txt")
+	file, err := os.Open("data2.txt")
 	if err != nil  {
 		fmt.Println(err)
 	}
-
-	scn := bufio.NewScanner(readFile)
-	scn.Split(bufio.ScanLines)
-	var line string
-	var sum int = 0
-	gamePatt := `Game\s*(\d+):`
-	numPatt := `\b\d+\b`
-	wordPatt := `\b([a-zA-Z]+)\b`
-	var gameNum = regexp.MustCompile(gamePatt)
-	var rgxnumber = regexp.MustCompile(numPatt)
-	var rgxword = regexp.MustCompile(wordPatt)
-	for scn.Scan(){
-		line = scn.Text()
-		fmt.Println(line)
-		
-		
-
-	}
-	fmt.Println(sum)
-	readFile.Close()
+	defer file.Close()
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+	//part1(lines[0:3])
+	//part1(lines[1:6])
+	part1(lines)
 }
